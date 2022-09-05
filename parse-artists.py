04717a171b262827f49ec4be6af8c11143eb3b72
Jan S <jan@js94x.de>
2022-09-05 18:29:36 +0200

    add parse-artists.py

---
+++ parse-artists.py
@@ -0,0 +1,21 @@
+import json
+
+artist_list = []
+
+with open('output.json') as a:
+    loaded = json.load(a)
+
+for t in loaded["playlists"][0]["tracks"]:
+    for a in t["track"]["artists"]:
+        artist_list.append(a["name"])
+
+unique_artist_list = list(set(artist_list))
+
+for ua in unique_artist_list:
+    f = open('./docs/artists/' + ua.replace('/', '_') + '.md', "a")
+    for t in loaded["playlists"][0]["tracks"]:
+        for a in t["track"]["artists"]:
+            if a["name"] == ua:
+                f.write(t["track"]["name"] + "\n")
+                # print(ua + " has " + t["track"]["name"])
+    f.close()
