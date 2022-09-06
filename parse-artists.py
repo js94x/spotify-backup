1d99a56b20a8fb8bbd8f07cce5a50ab0e67358d4
Jan S <jan@js94x.de>
2022-09-06 07:48:27 +0200

    add artist feature & debug out

--- parse-artists.py
+++ parse-artists.py
@@ -12,10 +12,16 @@ for t in loaded["playlists"][0]["tracks"]:
 unique_artist_list = list(set(artist_list))
 
 for ua in unique_artist_list:
+    print("Artist: " + ua + "\n")
     f = open('./docs/artists/' + ua.replace('/', '_') + '.md', "a")
+    f.write(
+    "# " + ua + "\n\n"
+    "|Track|Artist|\n"
+    "|-----|----|\n"
+    )
     for t in loaded["playlists"][0]["tracks"]:
         for a in t["track"]["artists"]:
             if a["name"] == ua:
-                f.write(t["track"]["name"] + "\n")
+                f.write("|" + t["track"]["name"] + "|" + ua + "|\n")
                 # print(ua + " has " + t["track"]["name"])
     f.close()
